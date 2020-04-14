6b8a2fc3a65a6b6d5e515f6bfde0ba65c737fcb3
jesselsmith <jesse.lee.smith@gmail.com>
2020-04-13 21:24:49 -0700

    continuing to set up basics, now adding serializers and controllers for backend api

--- inventory-backend/app/models/character.rb
+++ inventory-backend/app/models/character.rb
@@ -1,4 +1,17 @@
 class Character < ApplicationRecord
   has_many :owned_items
   has_many :slots
+
+  def max_slots
+    this.strength*2
+  end
+
+  def encumbered
+    this.strength
+  end
+
+  def heavily_encumbered
+    this.strength + 3
+  end
+  
 end
---
+++ inventory-backend/db/schema.rb
@@ -0,0 +1,56 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_04_13_222240) do
+
+  create_table "base_items", force: :cascade do |t|
+    t.string "name"
+    t.string "category"
+    t.integer "slots"
+    t.string "image"
+    t.integer "contained_slots"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "characters", force: :cascade do |t|
+    t.string "name"
+    t.integer "strength"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "owned_items", force: :cascade do |t|
+    t.integer "character_id", null: false
+    t.integer "base_item_id", null: false
+    t.integer "notches"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["base_item_id"], name: "index_owned_items_on_base_item_id"
+    t.index ["character_id"], name: "index_owned_items_on_character_id"
+  end
+
+  create_table "slots", force: :cascade do |t|
+    t.string "kind"
+    t.integer "character_id", null: false
+    t.integer "owned_item_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["character_id"], name: "index_slots_on_character_id"
+    t.index ["owned_item_id"], name: "index_slots_on_owned_item_id"
+  end
+
+  add_foreign_key "owned_items", "base_items"
+  add_foreign_key "owned_items", "characters"
+  add_foreign_key "slots", "characters"
+  add_foreign_key "slots", "owned_items"
+end

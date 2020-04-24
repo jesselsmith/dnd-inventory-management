696a9b97aeb37f14753eb9b6f3e1bc5e841efd06
jesselsmith <jesse.lee.smith@gmail.com>
2020-04-23 17:46:24 -0700

    starting work on character page/inventory

---
+++ inventory-frontend/src/components/character/characterInventory.js
@@ -0,0 +1,20 @@
+
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import WornInventory from '../slot/wornInventory'
+import CarriedInventory from '../slot/carriedInventory'
+import ContainerInventory from '../slot/containerInventory'
+
+class CharacterInventory extends Component {
+  render(){
+    return(
+      <>
+        <WornInventory />
+        <CarriedInventory />
+        <ContainerInventory />
+      </>
+    )
+  }
+}
+
+export default connect()(CharacterInventory)

ef21fa8771d37ed74a1f463f6466cc3d0a6f012e
jesselsmith <jesse.lee.smith@gmail.com>
2020-05-20 13:01:09 -0700

    starting to implement container inventory, beginning with having a form to add new containers

---
+++ inventory-frontend/src/components/slot/addContainerForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AddContainerForm extends Component {
+  render(){
+    return(
+      <>
+        Form
+      </>
+    )
+  }
+}
+
+export default AddContainerForm
--- inventory-frontend/src/components/slot/containerInventory.js
+++ inventory-frontend/src/components/slot/containerInventory.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux'
+import AddContainerForm from './addContainerForm'
 
-const containerInventory = props => {
-  return (
-    <div>
-      <h4>Container Inventory</h4>
-    </div>
-  )
+class containerInventory extends Component {
+  state = {
+    showAddContainerForm: false
+  }
+
+  handleAddContainer = () => {
+    this.setState({
+      showAddContainerForm: true
+    })
+  }
+
+  showButtonOrForm = () => {
+    if(this.state.showAddContainerForm){
+      return <AddContainerForm />
+    }else{
+      return <button onClick={this.handleAddContainer}>Add Container</button>
+    }
+  }
+
+  render(){
+    return (
+      <div>
+        <h4>Container Inventory</h4>
+        {this.showButtonOrForm()}
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = state => ({
